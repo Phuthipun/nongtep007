6e7be8849215f42d571cc5000470af11e72d977e
Phuthipun <105832176+Phuthipun@users.noreply.github.com>
2023-12-19 19:39:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// JavaScript code to simulate CCTV camera feed
+// You can replace this with actual video source or stream
+
+// Get the video element
+const video = document.getElementById('camera');
+
+// Sample video URL (replace with your own video URL or source)
+const videoURL = '724590808.232984.mp4';
+
+// Set the video source
+video.src = videoURL;
